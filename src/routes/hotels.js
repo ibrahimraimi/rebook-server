3f95eccb4a9d376e89d9f0edb3cf808a1a1c7fda
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-07-01 18:49:47 +0100

    feat: create hotel routes

---
+++ src/routes/hotels.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotels,
+  getHotel,
+  updateHotel,
+} from "../controllers/hotelController.js";
+
+const router = express.Router();
+
+router.post("/", createHotel);
+router.put("/:id", updateHotel);
+router.delete("/:id", deleteHotel);
+router.get("/:id", getHotel);
+router.get("", getAllHotels);
+
+export default router;
