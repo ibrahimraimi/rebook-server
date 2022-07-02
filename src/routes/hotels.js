e92ead21a467da619b11cf4fedf9af4106a7e401
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-07-02 18:48:32 +0100

    refactor: update hotel route

--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -1,18 +1,34 @@
 import express from "express";
 import {
+  countByCity,
+  countByType,
   createHotel,
   deleteHotel,
-  getAllHotels,
   getHotel,
+  getHotelRooms,
+  getHotels,
   updateHotel,
-} from "../controllers/hotelController.js";
+} from "../controllers/hotel.js";
+import Hotel from "../models/HotelModel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/", createHotel);
-router.put("/:id", updateHotel);
-router.delete("/:id", deleteHotel);
-router.get("/:id", getHotel);
-router.get("", getAllHotels);
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
