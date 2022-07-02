205a2f004e5a2c3e5c495b305c9d5d267b496022
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-07-02 18:48:00 +0100

    feat: create index application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log("Hello World")
+import express from "express";
+import dotenv from "dotenv";
+import connection from "./database/database.js";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//! Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser);
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/hotels", hotelsRoute);
+app.use("/api/v1/rooms", roomsRoute);
+app.use("/api/v1/users", usersRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(PORT, () => {
+  connection();
+  console.log(`Server running on http://127.0.0.0.1:${PORT}`);
+});
